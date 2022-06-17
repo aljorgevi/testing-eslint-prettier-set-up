685b71e0e72b331e889a15b2f010c96e77f9e211
aljorgevi <jormencar@yahoo.com>
2022-06-17 17:03:30 +0100

    import jest-dom

--- src/tests/Greeting.test.js
+++ src/tests/Greeting.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Greeting from "../components/Greeting";
+import "@testing-library/jest-dom";
 
 describe("Greeting component", () => {
 	test("renders Hello World as a text", () => {
