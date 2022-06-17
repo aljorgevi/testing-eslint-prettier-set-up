9403edb59280b4d46c1ccb07cbce7bbda1e08d59
aljorgevi <jormencar@yahoo.com>
2022-06-17 23:15:49 +0100

    re-factor tests

--- src/tests/FirstApp.test.jsx
+++ src/tests/FirstApp.test.jsx
@@ -1,28 +1,37 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import FirstApp from "../components/FirstApp";
 
-describe("Test of <FirstApp />", () => {
+describe("match Snapshot <FirstApp />", () => {
+	const title = "Hello, I'm Goku";
+	const subtitle = "Hello World";
+
 	test.skip("must match eith snapshot", () => {
-		const title = "Hello World";
 		const { container } = render(<FirstApp title={title} />);
 		expect(container).toMatchSnapshot();
 	});
 
 	test("Should show title and data-testid", () => {
-		const title = "Hello World";
-		// eslint-disable-next-line no-unused-vars
-		const { container, getByText, getByTestId } = render(<FirstApp title={title} />);
-		expect(getByText(title)).toBeInTheDocument();
-		expect(getByTestId("test-title")).toHaveTextContent(title);
+		render(<FirstApp title={title} />);
+		expect(screen.getByText(title)).toBeTruthy();
+		// screen.debug();
+		// const title = "Hello World";
+		// // eslint-disable-next-line no-unused-vars
+		// const { container, getByText, getByTestId } = render(<FirstApp title={title} />);
+		// expect(getByText(title)).toBeInTheDocument();
+		// expect(getByTestId("test-title")).toHaveTextContent(title);
 
 		// const h1 = container.querySelector("h1");
 		// expect(h1.innerHTML).toBe(title);
 	});
 
+	test("should show title in a h1", () => {
+		render(<FirstApp title={title} />);
+		expect(screen.getByRole("heading", { level: 1 }).innerHTML).toContain(title);
+	});
+
 	test("should show the subtitle send by props", () => {
-		const subtitle = "Hello World";
-		const { getByText } = render(<FirstApp subTitle={subtitle} />);
-		expect(getByText(subtitle)).toBeInTheDocument();
+		render(<FirstApp subTitle={subtitle} />);
+		expect(screen.getByText(subtitle)).toBeTruthy();
 	});
 });
