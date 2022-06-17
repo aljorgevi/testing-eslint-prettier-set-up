c3e558d115e7d658b327967ca530409f00f4c888
aljorgevi <jormencar@yahoo.com>
2022-06-17 19:41:02 +0100

    add async test

---
+++ src/tests/Async.test.js
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Async from "../components/Async";
+
+describe("Async component", () => {
+	test("renders post if request succeeds", async () => {
+		// Arrange
+		window.fetch = jest.fn().mockResolvedValueOnce({
+			json: async () => {
+				return [
+					{
+						id: "p1",
+						title: "first post",
+					},
+				];
+			},
+		});
+		render(<Async />);
+
+		// Act
+		// .. nothing
+
+		// Assert
+		const listItemElements = await screen.findAllByRole(
+			"listitem"
+			// {},
+			// {
+			// 	timeout: 5000,
+			// }
+		);
+		expect(listItemElements).not.toHaveLength(0);
+	});
+});
