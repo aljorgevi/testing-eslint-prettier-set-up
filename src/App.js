3f939a0d0ff39c13d400ae2b58f8a264cea0fd0d
aljorgevi <jormencar@yahoo.com>
2022-06-18 00:33:58 +0100

    render gifts app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // import FirstApp from "./components/FirstApp";
 
-import { CounterApp } from "./components/CounterApp";
+// import { CounterApp } from "./components/CounterApp";
+import { GifExpertApp } from "./GifExpertApp";
 
 function App() {
 	return (
 		<div className="App ">
 			{/* <FirstApp title="Hello, I'm Vegeta" /> */}
-			<CounterApp value={0} />
+			<GifExpertApp />
 		</div>
 	);
 }
