7414fc6a30398fe8ca44a439cb536e29e195e938
aljorgevi <jormencar@yahoo.com>
2022-06-18 12:06:31 +0100

    import gift mocks from directory

--- src/tests/Components/GifGrid.test.jsx
+++ src/tests/Components/GifGrid.test.jsx
@@ -1,14 +1,25 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../components/GifGrid";
+import { useFetchGifs } from "../../hooks/useFetchGifs";
+import { gifts } from "../__mocks__/gifts";
+
+jest.mock("../../hooks/useFetchGifs");
 
 describe("test for <GifGrid />", () => {
 	const category = "england";
 
 	test("should start with loading", () => {
+		useFetchGifs.mockReturnValue({ images: [], isLoading: true });
 		render(<GifGrid category={category} />);
 
 		expect(screen.getByText("Loading...")).toBeInTheDocument();
 	});
 
-	test("should show images when data is ready", () => {});
+	test("should show images when data is ready", () => {
+		useFetchGifs.mockReturnValue({ images: gifts, isLoading: false });
+
+		render(<GifGrid category={category} />);
+		expect(screen.getAllByRole("img")).toHaveLength(2);
+		// screen.debug();
+	});
 });
