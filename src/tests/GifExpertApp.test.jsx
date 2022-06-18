7a80800acf6f2fbd45e9cb23e64b19ab632747b4
aljorgevi <jormencar@yahoo.com>
2022-06-18 12:50:25 +0100

    add test for giftExpertApp

---
+++ src/tests/GifExpertApp.test.jsx
@@ -0,0 +1,14 @@
+// import { render, screen, waitFor } from "@testing-library/react";
+// import { GifExpertApp } from "../GifExpertApp";
+
+describe("test for <GifExpertApp />", () => {
+	test("should render", async () => {
+		// TODO:
+		// render(<GifExpertApp />);
+		// const loadingEl = screen.getByText("Loading...");
+		// await waitFor(() => expect(loadingEl).toBeNull());
+		// // screen.debug();
+		// const images = screen.getAllByRole("img");
+		// expect(images.length).toBeGreaterThan(0);
+	});
+});
