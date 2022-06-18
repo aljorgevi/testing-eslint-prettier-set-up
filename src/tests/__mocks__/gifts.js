1aaa7fed1e000c307977c62bafa54b531f810da8
aljorgevi <jormencar@yahoo.com>
2022-06-18 12:06:21 +0100

    add gifts mock

---
+++ src/tests/__mocks__/gifts.js
@@ -0,0 +1,12 @@
+export const gifts = [
+	{
+		id: "ABC",
+		url: "https://media2.giphy.com/media/l0MYBQYXQXZ2w/giphy.gif",
+		title: "Gif 1",
+	},
+	{
+		id: "DEF",
+		url: "https://media2.giphy.com/media/l0MYBQYXQXZ2w/giphy.gif",
+		title: "Gif 2",
+	},
+];
