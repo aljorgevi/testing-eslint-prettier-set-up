1029058dafaa25f513f6afd4a37623f45e3a99e1
aljorgevi <jormencar@yahoo.com>
2022-06-18 11:22:25 +0100

    add test for forms

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -9,6 +9,7 @@ export const AddCategory = ({ onNewCategory }) => {
 	};
 
 	const onSubmit = event => {
+		console.log("testing hello world from submit");
 		event.preventDefault();
 		if (inputValue.trim().length <= 1) return;
 
